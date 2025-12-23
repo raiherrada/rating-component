865d28bb14d5f191d98e9a860b228c5ea8498351
raiherrada <rainieroherrada@gmail.com>
2025-12-23 20:00:46 +0100

    Deleted comments

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-/* Initial state
-
-Rating card visible
-
-No rating selected
-
-Selection state
-
-One rating button highlighted
-
-Submit enabled
-
-Submitted state
-
-Rating card hidden
-
-Thank-you card visible
-
-Selected rating displayed 
-
-“What happens if I press Enter?”
-
-“What happens if no rating is selected?”*/
-
 const submitEl = document.querySelector(".submit-btn")
 const hiddenEl = document.querySelector(".hidden-container")
 const container = document.querySelector(".container")
