c9dc2fc0f43fe2a4b2a567d48f18c11b29dbb382
raiherrada <rainieroherrada@gmail.com>
2025-12-23 02:37:10 +0100

    Uploading files

---
+++ script.js
@@ -0,0 +1,23 @@
+/* Initial state
+
+Rating card visible
+
+No rating selected
+
+Selection state
+
+One rating button highlighted
+
+Submit enabled
+
+Submitted state
+
+Rating card hidden
+
+Thank-you card visible
+
+Selected rating displayed 
+
+“What happens if I press Enter?”
+
+“What happens if no rating is selected?”*/
