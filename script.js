a9a870c292720d5d375778bf129cd8a6b474b97d
raiherrada <rainieroherrada@gmail.com>
2025-12-23 19:42:35 +0100

    Updated code

--- script.js
+++ script.js
@@ -21,3 +21,28 @@ Selected rating displayed
 “What happens if I press Enter?”
 
 “What happens if no rating is selected?”*/
+
+const submitEl = document.querySelector(".submit-btn")
+const hiddenEl = document.querySelector(".hidden-container")
+const container = document.querySelector(".container")
+const ratingButtons = document.querySelectorAll(".score-buttons button")
+const ratingText = document.getElementById("rating-text")
+
+submitEl.disabled = true
+
+for (let i = 0; i < ratingButtons.length; i++){
+    ratingButtons[i].addEventListener('click', function(){
+        const selectedRating = ratingButtons[i].textContent
+        localStorage.setItem("rating", selectedRating)
+        submitEl.disabled = false
+    })
+}
+
+submitEl.addEventListener('click', function(e){
+    e.preventDefault()
+    container.style.display = 'none'
+    hiddenEl.style.display = 'flex'
+
+    const rating = localStorage.getItem("rating")
+    ratingText.textContent = `You selected ${rating} out of 5`
+})
